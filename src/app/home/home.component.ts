4e20d001a8b1ba9aa44d58338f05b3682bc2a91a
Abraham Salazar <abraham_jack10@hotmail.com>
2024-04-22 21:36:01 -0500

    Update Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
