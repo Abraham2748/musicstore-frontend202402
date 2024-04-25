c08a6438c55a7861a9b7430a38785479347f7d7f
Abraham Salazar <abraham_jack10@hotmail.com>
2024-04-24 20:50:14 -0500

    Create Home Service

---
+++ src/app/home/home.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HomeApiResponse } from './home.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeService {
+  private http = inject(HttpClient);
+
+  private baseUrl = 'http://localhost:8080';
+
+  getData(): Observable<HomeApiResponse> {
+    const apiUrl = this.baseUrl + '/api/Home';
+    return this.http.get<HomeApiResponse>(apiUrl);
+  }
+}
