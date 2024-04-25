e2e536dea5ef24103d9d4d2b888ab9926ee4d0b5
Abraham Salazar <abraham_jack10@hotmail.com>
2024-04-24 19:50:48 -0500

    Implement Lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   {
@@ -10,12 +6,30 @@ export const routes: Routes = [
     pathMatch: 'full',
     redirectTo: 'home',
   },
-  { path: 'home', pathMatch: 'full', component: HomeComponent },
-  { path: 'login', pathMatch: 'full', component: LoginComponent },
-  { path: 'register', pathMatch: 'full', component: RegisterComponent },
+  {
+    path: 'home',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./register/register.component').then((m) => m.RegisterComponent),
+  },
   {
     path: 'forgot-password',
     pathMatch: 'full',
-    component: ForgotPasswordComponent,
+    loadComponent: () =>
+      import('./forgot-password/forgot-password.component').then(
+        (m) => m.ForgotPasswordComponent
+      ),
   },
 ];
