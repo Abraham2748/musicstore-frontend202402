86f16efa48cffc2555f0c88e19977e579c5f7a58
Abraham Salazar <abraham_jack10@hotmail.com>
2024-04-29 21:50:15 -0500

    Move header component to Home

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
