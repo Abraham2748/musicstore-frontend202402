a7eb12d4568679158aaf6bc29c2eb52ade75afe6
Abraham Salazar <abraham_jack10@hotmail.com>
2024-05-06 21:15:34 -0500

    Create Login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/components/footer/footer.component';
-import { HeaderComponent } from '../home/header/header.component';
 import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [SimpleHeaderComponent, FooterComponent],
+  imports: [
+    SimpleHeaderComponent,
+    FooterComponent,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+  ]);
+}
