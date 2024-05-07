31dc3ac87ad4439ce42d5dfe8310d7f8fced06c9
Abraham Salazar <abraham_jack10@hotmail.com>
2024-05-06 21:15:21 -0500

    Create Forgot Password form

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../shared/components/footer/footer.component';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [SimpleHeaderComponent, FooterComponent],
+  imports: [
+    SimpleHeaderComponent,
+    FooterComponent,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css',
 })
-export class ForgotPasswordComponent {}
+export class ForgotPasswordComponent {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+}
