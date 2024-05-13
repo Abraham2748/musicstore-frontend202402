71e49ad624845631de8ef9b1cc38e7047cad7ee6
Abraham Salazar <abraham_jack10@hotmail.com>
2024-05-13 16:02:02 -0500

    Update Logged In Header layout

--- src/app/shared/components/logged-in-header/logged-in-header.component.ts
+++ src/app/shared/components/logged-in-header/logged-in-header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-logged-in-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './logged-in-header.component.html',
-  styleUrl: './logged-in-header.component.css'
+  styleUrl: './logged-in-header.component.css',
 })
-export class LoggedInHeaderComponent {
-
-}
+export class LoggedInHeaderComponent {}
