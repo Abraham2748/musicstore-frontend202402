9d6a3e4951f3355adc31059d04e8c175193176ba
Abraham Salazar <abraham_jack10@hotmail.com>
2024-05-13 15:22:11 -0500

    Create My Purchases component

---
+++ src/app/customer/my-purchases/my-purchases.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyPurchasesComponent } from './my-purchases.component';
+
+describe('MyPurchasesComponent', () => {
+  let component: MyPurchasesComponent;
+  let fixture: ComponentFixture<MyPurchasesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyPurchasesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyPurchasesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/customer/my-purchases/my-purchases.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-purchases',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-purchases.component.html',
+  styleUrl: './my-purchases.component.css'
+})
+export class MyPurchasesComponent {
+
+}
