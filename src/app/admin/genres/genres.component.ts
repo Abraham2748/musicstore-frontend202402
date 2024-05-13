23fb131263c26a0d640674bd63b360cf42229dee
Abraham Salazar <abraham_jack10@hotmail.com>
2024-05-13 15:18:54 -0500

    Create Genres component

---
+++ src/app/admin/genres/genres.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genres',
+  standalone: true,
+  imports: [],
+  templateUrl: './genres.component.html',
+  styleUrl: './genres.component.css'
+})
+export class GenresComponent {
+
+}
