1d5848f33b546c877af17913318c58f23aefa498
Abraham Salazar <abraham_jack10@hotmail.com>
2024-05-15 22:09:30 -0500

    Implement pdfmake library

--- src/app/admin/reports/reports.component.ts
+++ src/app/admin/reports/reports.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { BaseChartDirective } from 'ng2-charts';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-reports',
@@ -20,4 +24,67 @@ export class ReportsComponent {
     },
   ];
   public pieChartLegend = true;
+
+  createPdf() {
+    const content: TDocumentDefinitions = {
+      content: [
+        {
+          text: 'Reporte de ventas',
+          style: 'header',
+        },
+        {
+          text: 'Ventas por categoría',
+          style: 'subheader',
+        },
+        {
+          style: 'tableExample',
+          table: {
+            body: [
+              ['Categoría', 'Ventas'],
+              ['Download Sales', 300],
+              ['In-Store Sales', 500],
+              ['Mail-Order Sales', 100],
+            ],
+          },
+        },
+        {
+          text: 'Ventas por producto',
+          style: 'subheader',
+        },
+        {
+          style: 'tableExample',
+          table: {
+            body: [
+              ['Producto', 'Ventas'],
+              ['Product 1', 300],
+              ['Product 2', 500],
+              ['Product 3', 100],
+            ],
+          },
+        },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        subheader: {
+          fontSize: 16,
+          bold: true,
+          margin: [0, 10, 0, 5],
+        },
+        tableExample: {
+          margin: [0, 5, 0, 15],
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 13,
+          color: 'black',
+        },
+      },
+    };
+
+    pdfMake.createPdf(content).open();
+  }
 }
